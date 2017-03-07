d74bbf79fc58edf755e1a6e933d0ce82b3e9e98a
Tim Kreutz <tjkreutz@gmail.com>
2017-03-07 18:21:53 +0100

    Corrected the keywords script by using english stopwords

--- scripts/keywords.py
+++ scripts/keywords.py
@@ -8,15 +8,14 @@ def main(directory):
     allposts = posts.read_posts(directory)
     alltexts = [post.get_text() for post in allposts]
     
-    vec = TfidfVectorizer()
+    vec = TfidfVectorizer(stop_words='english')
     tfidf = vec.fit_transform(alltexts)
     features = vec.get_feature_names()
-    
     postid = 0
     
     for doc in tfidf:
         zipped = zip(doc.data, doc.indices)
-        sortedwords = [features[ind] for data, ind in sorted(zipped)]
+        sortedwords = [features[ind] for data, ind in reversed(sorted(zipped))]
         topfive = sortedwords[:5]
         curpost = allposts[postid]
         curpost.add_meta(['keywords'] + topfive)
