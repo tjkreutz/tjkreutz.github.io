4bd9780a95e3f388e953bdd0ec00bab45d544179
Tim Kreutz <tjkreutz@gmail.com>
2022-08-12 13:10:43 +0200

    Simplify plog.py and work with date argument

--- scripts/plog.py
+++ scripts/plog.py
@@ -1,20 +1,10 @@
 import os
+import sys
 import requests
 import datetime
 from httplib2 import Http
 from googleapiclient.discovery import build
 from oauth2client import file, client, tools
-
-def get_window(days=1):
-    now = datetime.datetime.now()
-    five = now.replace(hour=5, minute=0, second=0, microsecond=0)
-    if now.hour > 19: # window is today
-        start = five
-        end = now
-    else: # window is yesterday
-        start = five - datetime.timedelta(days=days)
-        end = five
-    return start, end
 
 def connect():
     scope = 'https://www.googleapis.com/auth/photoslibrary.readonly'
@@ -33,12 +23,11 @@ def download_file(url, outfile):
             f.write(response.content)
             f.close()
             
-def write_header(outfile, title, no):
+def write_header(outfile, title):
     outfile.write("---\n")
     outfile.write("layout: plog\n")
     outfile.write(f'title: "{title}"\n')
     outfile.write(f'date: {title}\n')
-    outfile.write(f'pictures: {str(no)}\n')
     outfile.write("exclude: true\n")
     outfile.write("---\n\n")
     outfile.write("class: middle, center\n")
@@ -52,40 +41,38 @@ def write_picture(outfile, path):
 def write_description(outfile, description):
     outfile.write(f"{description}\n\n")
 
-def main():
-    start, end = get_window()
-    title = start.strftime("%Y-%m-%d")
-    
+def main(datestring):
+    title = datestring
+    date = datetime.datetime.strptime(datestring, '%Y-%m-%d')
+
     api = connect()
     media_items = api.mediaItems()
-    query = media_items.search()
+    query = media_items.search(body={'filters': {'dateFilter': {'dates': [{'year': date.year, 'month': date.month, 'day': date.day}]}}})
     results = query.execute()
-    hits = []
+    
+    if not results:
+        return
+    
+    directory = os.path.join(f"img/plog/{title}")
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
-    for result in results['mediaItems']:
-        # filter outside window
-        creation_time =  result['mediaMetadata']['creationTime']
-        creation_datetime = datetime.datetime.strptime(creation_time, '%Y-%m-%dT%H:%M:%SZ')
-        if start < creation_datetime < end:
-            hits.append(result)
-     
-    hits.reverse()
     with open(f"_plog/{title}.md", "w") as outfile:
-        write_header(outfile, title, len(hits))
-        for i, hit in enumerate(hits, start=1):
+        
+        write_header(outfile, title)
+        
+        for i, result in enumerate(reversed(results['mediaItems']), start=1):
             istr = '0' + str(i) if i < 10 else str(i)
-
-            description = hit['description']
-            directory = os.path.join(f"img/plog/{title}")
-            extension = hit['filename'].split(".")[-1]
+            description = result['description']
+            extension = result['filename'].split(".")[-1]
             path = os.path.join(f"{directory}/{istr}.{extension}")
-
-            if not os.path.exists(directory):
-                os.mkdir(directory)
-
-            download_file(hit['baseUrl'], path)
+            download_file(result['baseUrl'], path)
 
             write_picture(outfile, path)
             write_description(outfile, description)
 
-main()
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Error: wrong number of arguments. Usage: ./plog.py <DATE>')
+    else:
+        main(sys.argv[1])
