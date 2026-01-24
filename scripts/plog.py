bc2f45d3cda9235057d8de9c77db81157c730f2f
tjkreutz <tjkreutz@gmail.com>
2026-01-24 23:25:28 +0100

    feat: Change plog script to use exports rather than api

--- scripts/plog.py
+++ scripts/plog.py
@@ -1,27 +1,8 @@
 import os
 import sys
-import requests
 import datetime
-from httplib2 import Http
-from googleapiclient.discovery import build
-from oauth2client import file, client, tools
-
-def connect():
-    scope = 'https://www.googleapis.com/auth/photoslibrary.readonly'
-    store = file.Storage('plog.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', scope)
-        creds = tools.run_flow(flow, store)
-    api = build('photoslibrary', 'v1', http=creds.authorize(Http()), static_discovery=False)
-    return api
-
-def download_file(url, outfile):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(outfile, 'wb') as f:
-            f.write(response.content)
-            f.close()
+import zipfile
+from pathlib import Path
             
 def write_header(outfile, title):
     outfile.write("---\n")
@@ -41,38 +22,54 @@ def write_picture(outfile, path):
 def write_description(outfile, description):
     outfile.write(f"{description}\n\n")
 
-def main(datestring):
-    title = datestring
-    date = datetime.datetime.strptime(datestring, '%Y-%m-%d')
+def extract_date_from_filename(filename):
+    try:
+        _, rawdate, _ = filename.split('_')
+        return datetime.datetime.strptime(rawdate, "%Y%m%d").strftime("%Y-%m-%d")
+    except ValueError:
+        return None
 
-    api = connect()
-    media_items = api.mediaItems()
-    query = media_items.search(body={'filters': {'dateFilter': {'dates': [{'year': date.year, 'month': date.month, 'day': date.day}]}}})
-    results = query.execute()
-    
-    if not results:
+def extract_dates_from_namelist(namelist):
+    dates = set()
+    for filename in namelist:
+        if date := extract_date_from_filename(filename):
+            dates.add(date)
+    return dates
+
+def process_zip_file(zip_path):
+    zip_ref = zipfile.ZipFile(zip_path, 'r')
+    namelist = zip_ref.namelist()
+    dates = extract_dates_from_namelist(namelist)
+
+    if len(dates) != 1:
+        print(f"Error: multiple dates found in {zip_path}: {dates}")
         return
-    
-    directory = os.path.join(f"img/plog/{title}")
-    if not os.path.exists(directory):
-        os.mkdir(directory)
+
+    title = dates.pop()
+    directory = Path(f"img/plog/{title}")
+    directory.mkdir(parents=True, exist_ok=True)
+        
+    zip_ref.extractall(directory)
 
     with open(f"_plog/{title}.md", "w") as outfile:
-        
         write_header(outfile, title)
         
-        for i, result in enumerate(reversed(results['mediaItems']), start=1):
-            istr = '0' + str(i) if i < 10 else str(i)
-            description = result['description']
-            extension = result['filename'].split(".")[-1]
-            path = os.path.join(f"{directory}/{istr}.{extension}")
-            download_file(result['baseUrl'], path)
+        for i, file in enumerate(os.listdir(directory), start=1):
+            _, ext = os.path.splitext(file.lower())
+                
+            istr = f"{i:02}"
+            source_path = directory / file
+            destination_path = directory / f"{istr}{ext}"
+            source_path.rename(destination_path)
+            write_picture(outfile, destination_path)
+            write_description(outfile, "")
 
-            write_picture(outfile, path)
-            write_description(outfile, description)
+def main(zip_files):
+    for zip_path in zip_files:
+        process_zip_file(zip_path)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('Error: wrong number of arguments. Usage: ./plog.py <DATE>')
+        print('Error: wrong number of arguments. Usage: ./plog.py <ZIP_FILE> [ZIP_FILE ...]')
     else:
-        main(sys.argv[1])
+        main(sys.argv[1:])
